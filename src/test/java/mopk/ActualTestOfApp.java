1d70bf8c1746f86eaa703f6b4f2b49efe607ad03
mopk <mopk2002@yandex.ru>
2014-06-30 11:06:45 +0700

    implementation of tests: parameterized 1st test/java/mopk/ActualTestOfApp.java

--- src/test/java/mopk/ActualTestOfApp.java
+++ src/test/java/mopk/ActualTestOfApp.java
@@ -6,20 +6,23 @@ package mopk;
 
 import org.apache.commons.math3.util.Precision;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class ActualTestOfApp {
 
+
     /**
      * Проверяет, действительно ли программа выдаёт результат деления
      * с точностью до второго знака.
      */
-    @Test
-    public void testRoundingToTwoDecimalPlaces() {
-        Float dividend = 32.1f;
-        Float divisor = 3.7f;
+    @Test(dataProvider = "a")
+    public void testRoundingToTwoDecimalPlaces(
+            Float dividend,
+            Float divisor
+    ) {
         List<String> outputAndErrorLines =
                 RunnerOfCalcDiv.runCalcDiv(
                     dividend.toString(),
@@ -41,6 +44,18 @@ public class ActualTestOfApp {
                             +
                         "(Actual precision is not 2 decimal place of accuracy)!"
         );
+    }
+
+
+    @SuppressWarnings("UnnecessaryLocalVariable")
+    @DataProvider(name = "a")
+    public Object[][] fetchParametersForPrecisionTest() {
+        Object[][] parameters =
+                new Object[][]{
+                        { 32.1f, 3.7f },
+                        { 2f, 3f }
+                };
+        return parameters;
     }
 
 }
